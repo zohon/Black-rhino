ab2a155d560273ef901485a4636b53ca38e6e379
Edouard Rabany <edouard.rabany@piksel.com>
2016-03-10 16:00:28 +0100

    Auth

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ var vulcanize = require('gulp-vulcanize');
 
 gulp.task('vulcanize', function() {
 	watch(['elements/*', 'elements/**/*'], function () {
+
+		var ladate=new Date()
+		var dateText = ladate.getHours()+":"+ladate.getMinutes()+":"+ladate.getSeconds()
+
+		console.log('['+dateText+']', 'Change in file');
+		
 		gulp.src('elements/elements.html')
 		.pipe(vulcanize({
 			stripComments: true,
